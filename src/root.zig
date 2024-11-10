1611f4e2b033ecd787cd372a98b4719ecb72dc22
Dadepo Aderemi <272535+dadepo@users.noreply.github.com>
2024-11-10 23:59:27 +0700

    Add decode

--- src/root.zig
+++ src/root.zig
@@ -31,11 +31,76 @@ pub fn encode(comptime T: type, number: T) [encoded_len(T)]u8 {
     return out;
 }
 
-test "various encode" {
+pub const DecodeError = error{ NotMinimal, Overflow, Insufficient };
+pub fn decode(comptime T: type, buf: []const u8) DecodeError!T {
+    var n: T = 0;
+    for (buf, 0..) |b, i| {
+        const k: u8 = @intCast(b & 0x7F);
+        n |= std.math.shl(T, k, (i * 7));
+        if ((b & 0x80) == 0) {
+            // last bit
+            if (b == 0 and i > 0) {
+                return error.NotMinimal;
+            }
+            return n;
+        }
+        if (i == (encoded_len(T) - 1)) {
+            return error.Overflow;
+        }
+    }
+    return error.Insufficient;
+}
+
+test "encode" {
     try std.testing.expectEqual(encode(u8, 1), [2]u8{ 1, 0 });
     try std.testing.expectEqual(encode(u16, 127), [3]u8{ 127, 0, 0 });
     try std.testing.expectEqual(encode(u16, 128), [3]u8{ 128, 1, 0 });
     try std.testing.expectEqual(encode(u16, 255), [3]u8{ 255, 1, 0 });
     try std.testing.expectEqual(encode(u16, 300), [3]u8{ 172, 2, 0 });
     try std.testing.expectEqual(encode(u16, 16384), [3]u8{ 128, 128, 1 });
+    try std.testing.expectEqual(encode(u64, std.math.maxInt(u64)), [10]u8{ 255, 255, 255, 255, 255, 255, 255, 255, 255, 1 });
+}
+
+test "decode" {
+    {
+        const buf = ([1]u8{1});
+        try std.testing.expectEqual(try decode(u8, buf[0..]), 1);
+    }
+    {
+        const buf = ([1]u8{0b0111_1111});
+        try std.testing.expectEqual(try decode(u8, buf[0..]), 127);
+    }
+    {
+        const buf = ([2]u8{ 0b1000_0000, 1 });
+        try std.testing.expectEqual(try decode(u8, buf[0..]), 128);
+    }
+    {
+        const buf = ([2]u8{ 0b1000_0000, 1 });
+        try std.testing.expectEqual(try decode(u8, buf[0..]), 128);
+    }
+    {
+        const buf = ([2]u8{ 0b1111_1111, 1 });
+        try std.testing.expectEqual(try decode(u8, buf[0..]), 255);
+    }
+    {
+        const buf = ([3]u8{ 0x80, 0x80, 1 });
+        try std.testing.expectEqual(try decode(u16, buf[0..]), 16384);
+    }
+    {
+        const buf = ([2]u8{ 0b1010_1100, 0b0000_0010 });
+        try std.testing.expectEqual(try decode(u16, buf[0..]), 300);
+    }
+    {
+        const buf = ([10]u8{ 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 1 });
+        try std.testing.expectEqual(try decode(u64, buf[0..]), 0xFFFFFFFFFFFFFFFF);
+    }
+    // errors.
+    {
+        const buf = ([10]u8{ 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80 });
+        try std.testing.expectError(DecodeError.Overflow, decode(u64, buf[0..]));
+    }
+    {
+        const buf = ([9]u8{ 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80 });
+        try std.testing.expectError(DecodeError.Insufficient, decode(u64, buf[0..]));
+    }
 }
